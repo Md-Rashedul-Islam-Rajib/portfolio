edf320a8a404953d8712c1374ca21cd8a6efe155
rajib5570 <rajib5570@gmail.com>
2024-06-24 17:46:45 +0600

    modified contact page

--- src/pages/contact/layout/Contact.jsx
+++ src/pages/contact/layout/Contact.jsx
@@ -38,11 +38,13 @@ const Contact = () => {
   };
 
   return (
-    <div>
+    <div className='grid grid-cols-2 justify-between w-full'>
         <div></div>
+
+
         <div>
 
-        <div className='text-2xl font-helvetica-neue'>
+        <div className='text-4xl font-helvetica-neue flex items-center h-full'>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
           <svg
@@ -54,28 +56,21 @@ const Contact = () => {
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
 
-          <input  type="text" placeholder="Name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"  {...register('Name')} />
+          <input  type="text" placeholder="Name" className="w-full py-2 placeholder:pl-6 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"  {...register('Name')} />
          
         </label>
 
         <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-  <input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" {...register('email', { required: true })} />
+  
+  <input id="email" type="email" placeholder="Email" className="w-full py-2 placeholder:pl-6 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" {...register('email', { required: true })} />
 				
 </label>
   {errors.email && <p className="text-red-600 text-sm">Email is required</p>}
 
   <label className="input input-bordered flex items-center gap-2 mb-2 md:mb-4">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className="w-4 h-4 opacity-70"
-  >
-    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-  </svg>
+  
   <textarea id="bio"
-   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
+   className="w-full py-2 placeholder:pl-6 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
    placeholder="Message"
     {...register('message')}
    ></textarea>
